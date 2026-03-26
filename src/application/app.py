d8a815f44609e89297b636aa3e54323a2a99f70e
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2026-03-26 14:54:29 -0500

    poc: engine hello world

--- src/application/app.py
+++ src/application/app.py
@@ -4,14 +4,20 @@ from fastapi import FastAPI
 
 from api.router import router as v1_router
 from infrastructure.config import settings
+from infrastructure.postgres import engine
 
 
 def create_app() -> FastAPI:
-    app = FastAPI(title=settings.app_name, lifespan=lifespan)
+    app = FastAPI(title=settings.APP_NAME, lifespan=lifespan)
     app.include_router(router=v1_router, prefix="/api/v1")
     return app
 
 
 @asynccontextmanager
 async def lifespan(_: FastAPI):
+    with engine.connect() as connection:
+        from sqlalchemy import text
+
+        result = connection.execute(statement=text("select 'hello world'"))
+        print(result.all())
     yield
