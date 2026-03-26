b41dd0d92f61105dac12adf2d5025f1a60626e37
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2026-03-26 14:54:05 -0500

    feat: postgres setup

--- src/infrastructure/config.py
+++ src/infrastructure/config.py
@@ -1,12 +1,32 @@
+from pydantic import PostgresDsn, computed_field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    app_name: str = "Python Backend Template"
-    environment: str = "development"
-    database_url: str = "sqlite:///./app.db"
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+    )
+    APP_NAME: str = "PythonBackendTemplate"
+    ENVIRONMENT: str = "development"
 
-    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
+    POSTGRES_USER: str = "postgres"
+    POSTGRES_PASSWORD: str = "postgres"
+    POSTGRES_HOST: str = "localhost"
+    POSTGRES_PORT: int = 5432
+    POSTGRES_DB: str = "db"
+
+    @computed_field
+    @property
+    def postgres_database_url(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+psycopg",
+            username=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            port=self.POSTGRES_PORT,
+            path=self.POSTGRES_DB,
+        )
 
 
 settings = Settings()
