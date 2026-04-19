1a8234c02b9bcb16edbfdd6ad38002aade46ab2f
Manuel Loaiza <manuel.loaiza.vasquez@gmail.com>
2026-04-19 14:05:27 -0500

    refactor: json_schema_extra over extra

--- src/api/schemas/auth_schemas.py
+++ src/api/schemas/auth_schemas.py
@@ -10,19 +10,19 @@ class RegisterRequest(BaseModel):
         description="A unique and human-readable handle used for identification.",
         min_length=1,
         max_length=32,
-        example=Examples.USERNAME,
+        json_schema_extra={"example": Examples.USERNAME},
     )
     password: str = Field(
         description="The secret string to authenticate the user. Must contain at least one number and one special character.",
         min_length=8,
         max_length=128,
-        example=Examples.PASSWORD,
+        json_schema_extra={"example": Examples.PASSWORD},
     )
     full_name: str = Field(
         description="The user preferred display name.",
         min_length=1,
         max_length=256,
-        example=Examples.FULL_NAME,
+        json_schema_extra={"example": Examples.FULL_NAME},
     )
 
     @field_validator("password")
--- src/api/schemas/user_schemas.py
+++ src/api/schemas/user_schemas.py
@@ -8,17 +8,17 @@ from api.schemas.examples import Examples
 class UserResponse(BaseModel):
     id: UUID = Field(
         description="Unique identifier for the object.",
-        example=Examples.UUID,
+        json_schema_extra={"example": Examples.UUID},
     )
     username: str = Field(
         description="A unique and human-readable handle used for identification.",
         min_length=1,
         max_length=32,
-        example=Examples.USERNAME,
+        json_schema_extra={"example": Examples.USERNAME},
     )
     full_name: str = Field(
         description="The user preferred display name.",
         min_length=1,
         max_length=256,
-        example=Examples.FULL_NAME,
+        json_schema_extra={"example": Examples.FULL_NAME},
     )
